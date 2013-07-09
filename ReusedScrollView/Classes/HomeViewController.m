44ede0e190cff58f8d1796c62bb6991c754aadc1
MIKE <boch2436@gmail.com>
2013-07-09 09:40:02 +0800

    创建文件

---
+++ ReusedScrollView/Classes/HomeViewController.m
@@ -0,0 +1,38 @@
+//
+//  HomeViewController.m
+//  ReusedScrollView
+//
+//  Created by Mike Chen on 13-7-9.
+//  Copyright (c) 2013年 BeyondSoft Co.,Ltd. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
