130d7a3cc20eca11160d594ec808775ff57f1fc8
MIKE <boch2436@gmail.com>
2013-07-09 09:37:43 +0800

    Initial Commit

---
+++ ReusedScrollView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ReusedScrollView
+//
+//  Created by Mike Chen on 13-7-9.
+//  Copyright (c) 2013年 BeyondSoft Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
